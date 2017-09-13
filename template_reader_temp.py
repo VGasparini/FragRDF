d0518d89643840b0b35cf119f701cfc7cd312c46
Vinicius Gasparini <viniciuszeiko@gmail.com>
2017-09-13 11:04:45 -0300

    added file creation function

--- template_reader_temp.py
+++ template_reader_temp.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('configure_frag.xml')
+tree = ET.parse('configurefrag.xml')
 root = tree.getroot()
 obj,prop,name,templ,temp,output = [],[],[],[],[],[]
 
@@ -17,8 +17,18 @@ t = list(set(templ))
 for i in range(len(t)):
 	for j in range(len(templ)):
 		if (t[i] == templ[j]):
-			temp.append(name[i])
+			temp.append(name[j])
 		output.append(temp)
-		del temp[:]
 for i in range(len(t)):
 	output[i] = sorted(list(set(output[i])))
+print(output[0])
+
+try:
+    nome_arquivo = 'part'
+    arquivo = open(nome_arquivo, 'r+')
+except:
+    arquivo = open(nome_arquivo, 'w+')
+for i in range(len(output[0])):
+	arquivo.writelines(output[0][i])
+	arquivo.writelines('\n')
+arquivo.close()
