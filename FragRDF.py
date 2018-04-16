de5d7a7f2ec777bbb87f9c130d469b27f7a38c2e
Vinicius Gasparini <viniciuszeiko@gmail.com>
2018-04-16 16:26:21 -0300

    Update FragRDF.py
    Reformuled the way of schema is interpreted

--- FragRDF.py
+++ FragRDF.py
@@ -1,71 +1,74 @@
-import rdflib																		
+import rdflib
 import xml.etree.ElementTree as ET
 from rdflib import Namespace
 import time
+from pprint import pprint
 
-def create_part(part,obj):														
+def create_part(part,name,obj):
+	for i in range(len(name)):
+		n = str(name[i]) + ".t" + str(part+1)
+		try:
+	   		file = open(name, 'r+')
+		except:
+			file = open(name, 'w+')
+		for i in range(len(obj)):
+			for j in range(len(obj[i])):
+				file.writelines(str(obj[i][j])+'\n')
+		file.close()
 
-	name = "part.t" + str(part+1)
+def create_log(log,op):
+
+	name = "log" + str(op) + ".txt"
 	try:
    		file = open(name, 'r+')
 	except:
 		file = open(name, 'w+')
-	for i in range(len(obj)):
-		for j in range(len(obj[i])):
-			file.writelines("<"+str(obj[i][j])+">")
-			file.writelines('\n')
+	file.writelines("\n\nTempo de execucao total: {:.3f}s\nTempo verificacao schema: {:.3f}s\nTempo coletar dados: {:.3f}s\nTempo de criacao dos arquivos: {:.3f}s".format(log[0],log[1],log[2],log[3]))
 	file.close()
 
-def reference(g):													
+def file_name(data):
+	name = []
+	for i in range(len(data)):
+		for j in range(len(data[i])):
+			if (data[i][j][0] == data[i][j][1]):
+				name.append(data[i][j][0][(data[i][j][0].rfind("/")+1):])
+	return name
+
+def reference(g):
 	end = g[(g.rfind("/")+1):]
 	ref = Namespace(g[:(g.rfind("/")+1):])
 	return ref[end]
 
-def template_read():
-	tree = ET.parse('configurefrag.xml')
+def template_reader(path):
+	tree = ET.parse(path)
 	root = tree.getroot()
 
-	obj,prop = [],[]
 	name,entity,template = [],[],[]
-	data = []
+	data1,data = [],[[],[],[],[],[],[]]
 
 	for item in root.iter('item'):
-		parent = list(item.attrib.items())
-		for i in range(len(parent)):
-			obj = parent[i][0]
-			prop = parent[i][1]
-			if (obj == 'template'):
-				template.append(prop)
-			elif (obj == 'entity'):
-				entity.append(prop)
-			elif (obj == 'name'):
-				name.append(prop)
+		data1.append(list(item.attrib.items()))
+	for i in range(len(data1)):
+		temp2 = data1[i][0][1] , data1[i][1][1]
+		data[int(data1[i][3][1][1:])-1].append(temp2)
+		if(not any(t == data1[i][0][1] for t in name)):
+			name.append(data1[i][0][1])
+		if(not any(t == data1[i][1][1] for t in entity)):
+			entity.append(data1[i][1][1])
+		if(not any(t == data1[i][3][1] for t in template)):
+			template.append(data1[i][3][1])
+	# Var -> data[template_index][object,predicate]
+	return data,template
 
-	# template	  object 		 predicate
-	#  ("T1")	("Product")		("producer")
-
-	template_index = sorted(list(set(template)))
-
-	for i in range(len(template_index)):
-		temp2 = []
-		for j in range(len(template)):
-			temp = []
-			if (template_index[i] == template[j]):
-				temp.append(entity[j])
-				temp.append(name[j])
-			if (not not temp): temp2.append(temp)
-			del temp
-		data.append(temp2)
-	del temp2
-	
-	# Var -> data[template_index[object,predicate]]
-
-	return data,template_index
+base_path = 'dataset.nt'
+schema_path = 'configurefrag.xml'
 
 g = rdflib.Graph()
-g.parse(opi[op],format='nt')
+g.parse(base_path,format='nt')
 
-data = []
+schema,template_index = template_reader(schema_path)
+
+data,name = [],file_name(schema)
 
 for index in range(len(template_index)):
 	objects,predicates = [],[]
@@ -74,19 +77,22 @@ for index in range(len(template_index)):
 	for instance in range(len(schema[index])):
 		objects.append(schema[index][instance][0])
 		predicates.append(schema[index][instance][1])
-	
 
 	temp = []
-	
+
 	for instance in range(len(predicates)):
-		
+
 		ref_predicate = reference(predicates[instance])
-		type = g.value(ref_predicate)
+		ref_object = reference(objects[instance])
 
 		temp.append(list(x for x in g.subject_objects(predicate=ref_predicate)))
-	
+		print(ref_predicate)
+		pprint(temp)
+		break
+	break
+
 	data.append(temp)
 
 
 for i in range(len(data)):
-	create_part(i,sorted(data[i]))
+	create_part(i,name,sorted(data[i]))
